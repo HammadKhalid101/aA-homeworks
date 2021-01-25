bc5d5f7221be37b73ca013343ad79bd42bdb3ff4
Hammad Khalid <hammaddotkhalid@gmail.com>
2021-01-25 00:21:04 -0500

    Ran tests on LRUCache

--- W5D1/LRU_Cache.rb
+++ W5D1/LRU_Cache.rb
@@ -13,7 +13,7 @@ class LRUCache
         if @cache.include?(el)
             @cache.delete(el)
             @cache << el
-        elsif count >= size
+        elsif count >= @max_size
             @cache.shift
             @cache << el
         else
@@ -31,3 +31,22 @@ class LRUCache
     # helper methods go here!
 
   end
+
+# johnny_cache = LRUCache.new(4)
+
+# johnny_cache.add("I walk the line")
+# johnny_cache.add(5)
+
+# p johnny_cache.count # => returns 2
+
+# johnny_cache.add([1,2,3])
+# johnny_cache.add(5)
+# johnny_cache.add(-5)
+# johnny_cache.add({a: 1, b: 2, c: 3})
+# johnny_cache.add([1,2,3,4])
+# johnny_cache.add("I walk the line")
+# johnny_cache.add(:ring_of_fire)
+# johnny_cache.add("I walk the line")
+# johnny_cache.add({a: 1, b: 2, c: 3})
+
+# p johnny_cache.show
